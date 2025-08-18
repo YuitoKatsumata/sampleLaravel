d38a72d6039bd3197e66b3ccb9a990056615b73d
yuito.katsumata <kyuito1212@gmail.com>
2025-08-18 22:01:30 +0900

    Fix corrupted object for hello.blade.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,4 +10,17 @@ class HelloController extends Controller
         $message = "Hello, World!";
         return view("hello", compact('message'));
     }
+
+    public function greet($name, $age) {
+        $message = 'Hello , ' . ucfirst($name) . "! You are ". $age . " years old.";
+        return view("hello", compact('message'));
+    }
+
+    public function queryHello(Request $request) {
+        $name = $request->query('name', 'Guest');
+        $age = $request->query('age', 'unknown');
+
+        $message = 'Hello, ' . ucfirst($name) . "! You age " . $age . '.';
+        return view('hello', compact('message'));
+    }
 }
